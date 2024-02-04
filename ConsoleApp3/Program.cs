637790a24460dbfd56084b6e06f441b48644e330
sonam6788 <Sonam8725@conestogac.on.ca>
2024-02-03 21:35:32 -0500

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+case 1:
+    hunger = hunger - 2; // decrease health
+    health = health + 1; // increase health
+    Console.WriteLine("decrease hunger ,slightly increased health");
+    break;
+case 2:
+    hunger = hunger + 1;//increase hunger
+    happiness = happiness + 1;//increase happiness
+    Console.WriteLine("increase happiness,slightly increases hapiness");
+    break;
